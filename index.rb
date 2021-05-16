19b0a6667d2be335a7d87a89c62bde7c03ae8f36
Vivian <yuweizhang88@gmail.com>
2021-05-16 19:44:02 +1000

    create menu 123 and else

--- index.rb
+++ index.rb
@@ -1,26 +1,30 @@
 group = []
 
 while true
+      puts "There are #{group.length} members in the group"
       puts "Press 1) to add a member to the group"
       puts "Press 2) to display a random group"
       puts "Press 3) to quite"
       #take input from users
-      menu_input = gets.chomp
+      menu_input = gets.chomp.to_i
 
-      case menu_input
-      when 1 
+
+    case menu_input
+    when 1 
         puts "Enter name:"
         name = gets.chomp
         group.push(name)
-      when 2
+        pp group
+    when 2
         puts "Random group output:"
         #TODO - Output random group
-      when 3
+    when 3
         #TODO - check they want to quite
         break
-       else
-
-      end
-
-
+    else
+        puts "Invalid input please try again"
+    end
+    sleep(1)
+    system "clear"  
 end
+puts "Goodbye, thanks for using."
